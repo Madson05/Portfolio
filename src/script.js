d4c123f35ce3d9abda715c111b99745553024d40
madson <madsonsilva2005@gmail.com>
2023-06-16 09:54:59 -0300

    habilidades atualizadas

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ function openMenu() {
   if (menuBar.style.width == "0px") {
     menuBar.style.display = "block";
     menuBar.style.width = "180px";
-    console.log("eai");
   } else {
     menuBar.style.display = "none";
     menuBar.style.width = "0px";
